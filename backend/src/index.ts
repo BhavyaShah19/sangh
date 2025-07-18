64198bf1cb58716652e9dd4554d91489411d943c
Bhavya Shah <bshah19112001@gmail.com>
2025-07-18 18:20:07 +0530

    added some more cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,8 +10,17 @@ const prisma = new PrismaClient();
 const app = express();
 const allowedOrigins = ["https://mahavirnagar-jain-sangh-fqxtkvpgj-bhavyashah19s-projects.vercel.app"];
 app.use(cors({
-  origin:allowedOrigins
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
 }));
+
+app.options("*", cors());
+
 app.use(express.json());
 
 app.post("/api/v1/booking", async (req, res) => {
@@ -76,8 +85,8 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.listen(3001, () => {
-    console.log("Server is running on port 3001")
-})
+// app.listen(3001, () => {
+//     console.log("Server is running on port 3001")
+// })
 
 export default app;
